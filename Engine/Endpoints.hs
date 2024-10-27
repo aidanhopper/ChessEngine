09d389a538e72d721caaaec96e43c61d54452631
Aidan Hopper <aidanhop1@gmail.com>
2024-10-26 18:59:49 -0700

    Reapply "worked on evaluate function"
    This reverts commit a70ed38c71b3625e13d7ae5a3d45ce5867107e8b.

--- Engine/Endpoints.hs
+++ Engine/Endpoints.hs
@@ -19,35 +19,50 @@ type API =
   "moves" :> Capture "fen" String :> Capture "square" String :> Get '[JSON] [String]
     :<|> "domove" :> Capture "fen" String :> Capture "moving" String :> Capture "target" String :> Get '[JSON] (Maybe [String])
     :<|> "bestmove" :> Capture "fen" String :> Get '[JSON] String
+    :<|> "startstring" :> Get '[JSON] String
 
 server :: Server API
 server =
   handleMoves
     :<|> handleDoMove
     :<|> handleBestMove
+    :<|> handleStartString
   where
     handleMoves :: String -> String -> Handler [String]
-    handleMoves fenString square =
-      return $
-        map (\(Algebraic x) -> x) $
-          generateSafeMoves (Algebraic square) fenString
+    handleMoves fenString square = return $ trace ("Possible moves: " ++ show out) out
+      where
+        out =
+          map (\(Algebraic x) -> x) $
+            generateSafeMoves (Algebraic square) fenString
 
     handleDoMove :: String -> String -> String -> Handler (Maybe [String])
     handleDoMove fenString moving target =
       if Algebraic target `elem` possibleMoves
-        then return $ Just $ doMove (Algebraic moving) (Algebraic target) fenString
+        then
+          return $
+            Just $
+              trace
+              ("Doing move " ++ moving ++ " to " ++ target ++ " " ++ show move) move
         else return Nothing
       where
         possibleMoves =
           generateSafeMoves (Algebraic moving) fenString
+        move = doMove (Algebraic moving) (Algebraic target) fenString
 
     handleBestMove :: String -> Handler String
-    handleBestMove fenString = return $ generateBestMove fenString
+    handleBestMove fenString = return $ trace ("Getting the best moves fen string " ++ show out) out
+      where
+        out = generateBestMove fenString
+
+    handleStartString :: Handler String
+    handleStartString = return $ trace (show out) out
+      where
+        out = "The start string: rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 app :: Application
 app = serve (Proxy :: Proxy API) server
 
-runAPI :: IO ()
-runAPI = do
+runServer :: IO ()
+runServer = do
   putStrLn "Running server on http://localhost:8080"
   run 8080 app
