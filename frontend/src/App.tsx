d96c3ce33e7ea54ef3d5ba60eb9b8a51f3fe0a73
Aidan Hopper <aidanhop1@gmail.com>
2024-11-25 12:12:26 -0800

    updated frontend again

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,19 +7,19 @@ const App = () => {
 
   return (
     <div className=" flex flex-col min-h-screen bg-white">
-      <div className="flex-auto z-10 fixed h-16 w-screen bg-gradient-to-r from-cyan-400/60 to-purple-400/60
+      <div className="flex-auto z-10 fixed h-16 w-screen bg-gradient-to-r from-cyan-500/80 to-purple-500/80
         content-center backdrop-blur-lg">
         <span className="container font-bold text-xl w-screen m-auto max-h-fit
-          flex flex-row items-center content-center">
+          flex flex-row items-center content-center text-white">
           <a href="/">
             <img
-              className="w-12 border-4 border-black rounded-lg hover:invert
-            hover:bg-white hover:border-white duration-100 ease-in-out mx-10"
+              className="w-10 border-2 border-black rounded-lg invert
+            hover:bg-black hover:bg-opacity-20 duration-100 ease-in-out mx-10"
               alt="home"
-              src="/assets/rb.png" />
+              src="/assets/pb.png" />
           </a>
-          <a className="flex border-black rounded-lg hover:bg-black
-            hover:text-white py-1 px-8" href="/">
+          <a className="flex border-black rounded-lg hover:bg-white hover:bg-opacity-20
+            hover:text-white py-1 px-8 italic" href="/">
             portfolio
           </a>
           <a
@@ -28,8 +28,8 @@ const App = () => {
             rel="noreferrer"
             target="_blank">
             <img
-              className="w-12 mr-10 p-1 hover:bg-white hover:invert rounded-lg
-              border-[4px] border-black hover:border-white ease-in-out duration-100"
+              className="w-10 mr-10 p-1 hover:bg-black hover:bg-opacity-20 rounded-lg
+              border-2 border-black ease-in-out duration-100 invert"
               src="/assets/github.png"
               alt="github" />
           </a>
